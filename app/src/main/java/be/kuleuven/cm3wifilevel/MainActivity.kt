dd84c64a780cc7144d312bf9c9fd6d43b2a5c232
Gilles Callebaut <callebaut.gilles@gmail.com>
2022-06-01 15:46:55 +0200

    first commit

---
+++ app/src/main/java/be/kuleuven/cm3wifilevel/MainActivity.kt
@@ -0,0 +1,197 @@
+package be.kuleuven.cm3wifilevel
+
+
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.io.PrintWriter
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
+    private var isWifiStatePermissionGranted = false
+    private var isWifiChangePermissionGranted = false
+    private var isFineLocationPermissionGranted = false
+    private var isCoarseLocationPermissionGranted = false
+    private var isWritePermissionGranted = false
+
+
+    private val permissionsList = arrayOf(
+        Manifest.permission.ACCESS_WIFI_STATE,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.CHANGE_WIFI_STATE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+    lateinit var wifiManager: WifiManager
+
+
+    private val filename = "CM3WifiLevel.txt"
+    lateinit var file: File
+    lateinit var pw: PrintWriter
+
+    lateinit var textview: TextView
+
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(contxt: Context?, intent: Intent?) {
+            val resultList = wifiManager.scanResults as ArrayList<ScanResult>
+            val sb = StringBuilder()
+            val sb_file = StringBuilder()
+
+            val SSIDs = ArrayList<String>()
+
+
+            for (res in resultList) {
+                // ignore other APs with the same SSID
+                if (SSIDs.contains(res.SSID))
+                    continue
+                sb_file.append("${System.currentTimeMillis()},${res.SSID},${res.level}")
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    sb_file.append(",${res.channelWidth}")
+                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    sb_file.append(",${res.wifiStandard}")
+                }
+                sb.appendLine("${res.SSID}, ${res.level}")
+                sb_file.appendLine()
+                SSIDs += res.SSID
+            }
+
+
+            pw.append(sb_file.toString())
+            textview.text = sb.toString()
+            wifiManager.startScan()
+        }
+
+    }
+
+    override fun onStop() {
+        pw.close()
+        super.onStop()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textview = findViewById(R.id.aps)
+
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                permissions.entries.forEach { Log.i(TAG, "${it.key}: ${it.value}") }
+                isWifiChangePermissionGranted = permissions[Manifest.permission.CHANGE_WIFI_STATE]
+                    ?: isWifiChangePermissionGranted
+                isWifiStatePermissionGranted = permissions[Manifest.permission.ACCESS_WIFI_STATE]
+                    ?: isWifiStatePermissionGranted
+                isCoarseLocationPermissionGranted =
+                    permissions[Manifest.permission.ACCESS_COARSE_LOCATION]
+                        ?: isCoarseLocationPermissionGranted
+                isFineLocationPermissionGranted =
+                    permissions[Manifest.permission.ACCESS_FINE_LOCATION]
+                        ?: isFineLocationPermissionGranted
+                isWritePermissionGranted = permissions[Manifest.permission.WRITE_EXTERNAL_STORAGE]
+                    ?: isWritePermissionGranted
+
+
+                if (isWifiChangePermissionGranted && isWifiStatePermissionGranted && isCoarseLocationPermissionGranted && isFineLocationPermissionGranted && isWritePermissionGranted) {
+                    startWifiScanning()
+                } else {
+                    Toast.makeText(this, "Not all request granted... :(", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        requestPermissions()
+
+
+    }
+
+    private fun requestPermissions() {
+        Log.i(TAG, "Checking permissions")
+
+        val permissionRequest: MutableList<String> = ArrayList()
+
+        for (permReq in permissionsList) {
+            val permission = ContextCompat.checkSelfPermission(
+                this,
+                permReq
+            )
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                Log.i(TAG, "Permission $permReq is denied")
+                permissionRequest.add(permReq)
+            } else {
+                Log.i(TAG, "$permReq is approved")
+                when (permReq) {
+                    Manifest.permission.ACCESS_WIFI_STATE -> isWifiStatePermissionGranted = true
+                    Manifest.permission.ACCESS_COARSE_LOCATION -> isCoarseLocationPermissionGranted =
+                        true
+                    Manifest.permission.ACCESS_FINE_LOCATION -> isFineLocationPermissionGranted =
+                        true
+                    Manifest.permission.CHANGE_WIFI_STATE -> isWifiChangePermissionGranted = true
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE -> isWritePermissionGranted = true
+                }
+            }
+
+        }
+
+        if (permissionRequest.isNotEmpty()) {
+            permissionLauncher.launch(permissionRequest.toTypedArray())
+        } else {
+            startWifiScanning()
+        }
+
+
+    }
+
+    private fun startWifiScanning() {
+        val path =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
+        file = File(path, filename)
+        file.parentFile?.mkdirs()
+        if (file.exists())
+            file.delete()
+        pw = PrintWriter(file)
+
+        Log.i(TAG, "Absolute Path to output file is: ${file.absolutePath}")
+
+        pw.append("time_ms,ssid,rssi")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            pw.append(",channel_width")
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            pw.append(", wifi_standard")
+        }
+        pw.appendLine()
+
+        wifiManager =
+            this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        registerReceiver(
+            broadcastReceiver,
+            IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        )
+
+        wifiManager.startScan()
+
+    }
+}
